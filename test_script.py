fe1b13da1a7a2d440b748a80f9097438e9f32509
atomz16 <ajclement23@ksu.edu>
2023-03-23 11:59:33 -0500

    New commit

---
+++ test_script.py
@@ -0,0 +1,40 @@
+import praw
+import time
+import threading
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import word_tokenize
+from prawcore.exceptions import Forbidden
+
+# create a Reddit instance with your app's client ID and secret key
+reddit = praw.Reddit(client_id='bo_GpmWm5LwHuu0WzqFBgA',
+                     client_secret='itpdoMbtNW6x5CxZSsoRuihRcXBQ4A',
+                     user_agent='atomz19')
+
+def analyze(post):
+    post = post.lower()
+    tokens = word_tokenize(post)
+    for i in range(0, len(tokens)-1):
+        if tokens[i] == 'kansas' and tokens[i+1] == 'state':
+            return True
+    return False
+
+comments_collected = 0
+subreddits_reached = 0
+print('Starting seach')
+start_time = time.time()
+try:
+    for subreddit in reddit.subreddits.search(query='College'):
+        if subreddit.over18 != True:
+            subreddits_reached+=1
+            print('Subreddit #' + str(subreddits_reached))
+            for submission in subreddit.search(query='Kansas State', time_filter='year'):
+                submission.comments.replace_more()
+                for comment in submission.comments.list():
+                    if analyze(comment.body) == True:
+                        comments_collected += 1
+except Forbidden:
+    print('End of search')
+end_time = time.time()
+elapsed_time = end_time - start_time
+print('Total comments collected = ' + str(comments_collected) + ' in ' + f"{elapsed_time: .2f}" + ' seconds')
